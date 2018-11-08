5f5996ded64c72628b4d518b9b714ea1f0bf539e
Chauncey Williams <chauncey@mylio.com>
2018-11-07 20:05:58 -0800

    Updated hello.

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mylochauncey/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, Mr. Williams.\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
